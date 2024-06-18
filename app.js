f0aa859ea4aad212c927f4d8aa5c6485acea6a1d
Andrey Grytsenko <63291871+GrytsenkoAndrey@users.noreply.github.com>
2024-06-18 16:14:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+(function() {
+  const buttons = document.querySelector('.counterBtn'),
+    counterElem = document.querySelector('#counter');
+  let counter = 0;
+
+  buttons.forEach(function (button) {
+    button.addEventListener('click',function () {
+      if (button.classList.contains('.prevBtn')) {
+        counter--;
+      } else if (button.classList.contains('.nextBtn')) {
+        counter++;
+      }
+
+      counterElem.textContent = count;
+
+      if (counter < 0) {
+        counterElem.style.color = 'red';
+      } else if (counter > 0) {
+        counterElem.style.color = 'green';
+      } else {
+        counterElem..style.color = '#333';
+      }
+    });
+  });
+});
